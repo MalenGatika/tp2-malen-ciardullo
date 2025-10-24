feed181a715a681276cfdff81c1b4839b59ad3f9
MalenGatika <malengatika@gmail.com>
2025-10-24 20:14:30 -0300

    cambio video fondo y estetica

--- js/audioControl.js
+++ js/audioControl.js
@@ -11,3 +11,20 @@ document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => btn.classList.remove("scale-105"), 200);
   });
 });
+
+//agrego esto por problemas con la repeticion del video en algunos navegadores
+
+const bgVideo = document.getElementById('bgVideo');
+
+// Reproduce el video al terminar (fallback por compatibilidad)
+bgVideo.addEventListener('ended', () => {
+  bgVideo.currentTime = 0;
+  bgVideo.play();
+});
+
+// Intento de autoplay en mobile
+document.addEventListener('DOMContentLoaded', () => {
+  bgVideo.play().catch(() => {
+    console.log("Autoplay bloqueado, se activará al interactuar");
+  });
+});
